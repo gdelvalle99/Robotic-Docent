d530120624d00d2b3c1e76d12fdae0bd19d089dc
Sherman Lee <Galerider1@live.com>
2020-11-29 14:11:39 -0800

    Revert "Set up navigation bar and sidebar"
    This reverts commit c1194bfe0f8611421fc3621b68a6803e521f7973.

--- map-editor/src/App.tsx
+++ map-editor/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import NavigationBar from './components/Navigation';
-import './assets/styles/site.scss';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <NavigationBar />
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
