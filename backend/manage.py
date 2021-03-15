5f24dddaa3744883c7ac1d417b9c42519ee2a13e
Kyle Respicio <kyle.b.respicio@gmail.com>
2021-03-15 08:06:02 -0700

    Manage.py now works regardless of the server file

--- backend/manage.py
+++ backend/manage.py
@@ -1,6 +1,14 @@
-from server import app, db
-from flask_script import Manager
+from flask_script import Flask, Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
 
 from Models import *
 
