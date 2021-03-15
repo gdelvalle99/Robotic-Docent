56549b7127669b13a5b0959389350e3ce75b3399
Matthew Alighchi <31544591+Malighchi@users.noreply.github.com>
2021-03-14 17:08:20 -0700

    commented code

--- robot-docent-mobile/src/questionare.js
+++ robot-docent-mobile/src/questionare.js
@@ -3,8 +3,12 @@ import axios from 'axios';
 import React from 'react';
 import _ from "lodash";
 import styled from 'styled-components';
+/*
 
-const Button = styled.button`
+*/
+
+//styles the button ui elements to look nice
+const Button = styled.button` 
   background-color: black;
   color: white;
   font-size: 20px;
@@ -14,6 +18,7 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
+//Button that controls the dispay of the current questions/answer for the tour
 const QuestionPingButton = function(props){
     const {text, onClick} = props;
     return(
@@ -22,6 +27,9 @@ const QuestionPingButton = function(props){
         </div>
     );
 }
+
+//behavior for question buttons
+//on click, textbox pops up with answet text
 const Row = function(props){
     const {question, answer, onClick, showAnswerFlag} = props;
     return (
@@ -41,25 +49,26 @@ export default class QAGenerator extends React.Component {
         qbutton: [
             {showQuestionsFlag: false, qButtonText: '"I Have A Question!"'}
         ],
-        rows: [
+        rows: [//default (bunk) questions
           {question: '"Question 1"', answer: 'Answer 1', showAnswerFlag: false},
           {question: '"Question 2"', answer: 'Answer 2', showAnswerFlag: false},
           {question: '"Question 3"', answer: 'Answer 3', showAnswerFlag: false}
         ],
-        oldQA: []  
+        oldQA: [] //flag for updating question boxes 
       };
 
     }
     componentDidMount() {
-      this.refreshQA = setInterval(
+      this.refreshQA = setInterval( //pings robot every second
         () => this.updateList(),
         1000
       );
     }
     componentWillUnmount() {
-      clearInterval(this.refreshQA);
+      clearInterval(this.refreshQA); //stops pinging robot on death
     }
 
+    //behavior for adding new question and answer boxes
     addRow = (Q, A) => {
       const rows = [...this.state.rows, 
                     {question: Q, answer: A, showAnswerFlag: false}
@@ -69,6 +78,7 @@ export default class QAGenerator extends React.Component {
       });
     }
     
+    //behavior to delete all QA boxes when updating list
     clearRows = () => {
       this.setState({
         rows: []
@@ -76,14 +86,14 @@ export default class QAGenerator extends React.Component {
     }
 
     updateList = async () => {
-      var newQA = await axios.get(`http://127.0.0.1:5001/server/qa`).
+      var newQA = await axios.get(`http://127.0.0.1:5001/server/qa`). //robot server
         catch( newQA => newQA);
-      const robot_not_found = newQA instanceof Error;
-      if(!robot_not_found && !_.isEqual(newQA.data, this.state.oldQA)){
+      const robot_not_found = newQA instanceof Error; //error only occurs when robot server not found
+      if(!robot_not_found && !_.isEqual(newQA.data, this.state.oldQA)){//checks that new QA data was pulled from robot server
         var QAs = newQA.data;
-        this.clearRows();
+        this.clearRows(); //clear old data
         QAs.QAList.forEach(QA => {
-          this.addRow(QA.question, QA.answer)
+          this.addRow(QA.question, QA.answer) //populate screen with new QA
         });
         this.setState({
           oldQA: newQA.data
@@ -91,6 +101,9 @@ export default class QAGenerator extends React.Component {
       }
     }
     
+    //behavior for clicking on Question box
+    //should populatescreen with associated asnwer
+    //hides answer on second click
     showAnswer = (idx) => {
       const rows = [...this.state.rows];
       rows[idx].showAnswerFlag = !rows[idx].showAnswerFlag;
@@ -98,6 +111,9 @@ export default class QAGenerator extends React.Component {
           rows: rows
       });
     }
+    //behavior of the "I Have A Question" button
+    //on click, populates screen with questions and change text to "Thank You!"
+    //clicking "Thank You!" hides QA boxes and changes text back
     showQuestions = () => {
         const qbutton = [...this.state.qbutton];
         const rows = [...this.state.rows];
@@ -116,6 +132,8 @@ export default class QAGenerator extends React.Component {
         });
     }
     
+    //handles real-time rendering of new QA
+    //handles proper placement of UI elements
     render(){
       return(
         <div>
