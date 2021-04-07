181c7f9104dba4195bafcba14a2f9c332afbf7ac
Sherman Lee <Galerider1@live.com>
2021-04-07 15:33:20 -0700

    Merge SidebarUpdates with map-analytics

--- map-editor/src/components/Navigation.js
+++ map-editor/src/components/Navigation.js
@@ -5,7 +5,8 @@ import { Button } from '@material-ui/core';
 import { Home } from '../pages/Home';
 import { Analytics } from '../pages/Analytics';
 import { Editor } from '../pages/Editor';
-import { Tours } from '../pages/Tours';
+import ProfileIcon from "./ProfileIcon";
+// import { ProfileIcon } from './ProfileIcon'
 
 export const NavigationBar = (props) => {
     return (
@@ -26,11 +27,13 @@ export const NavigationBar = (props) => {
                         <Button className="navigation-button" variant="contained" href="/tours">Tours</Button>
                         <Button className="navigation-button" variant="contained" href="/analytics">Analytics</Button>
                     </div>
+                    <div className="navigation-profile">
+                    <   ProfileIcon/>
+                    </div>
                 </nav>
 
                 <Route path="/" exact component={Home} />
                 <Route path="/map-editor" exact component={Editor} />
-                <Route path="/tours" exact component={Tours} />
                 <Route path="/analytics" exact component={Analytics} />
 
             </div>
--- map-editor/src/pages/Analytics.js
+++ map-editor/src/pages/Analytics.js
@@ -1,10 +1,63 @@
-import React from "react";
+import {useState} from "react";
+import Chart from "react-google-charts";
+import { CircularProgress } from "@material-ui/core";
+import ExhibitInfo from "../components/analytics/ExhibitInfo";
+import DailyInfo from "../components/analytics/DailyInfo";
+import RobotInfo from "../components/analytics/RobotInfo";
+import FormDisplay from "../components/analytics/FormDisplay";
 
 export const Analytics = () => {
+
+    const [display, setDisplay] = useState("Exhibit Information");
+
+    const handleChange = (event) => {
+        console.log(event.target.value, display === "Exhibit Information")
+        setDisplay(event.target.value);
+    };
+
     return (
-        <div>
-            This is Analytics
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "space-around",
+                // alignItems: "center",
+                // flexDirection: "column",
+            }}
+        >
+            <div style={{ display: "flex", maxWidth: 900 }}>
+                {/* <Chart
+                    width={400}
+                    height={300}
+                    chartType="ColumnChart"
+                    loader={<CircularProgress />}
+                    data={[
+                        ["City", "2010 Population", "2000 Population"],
+                        ["New York City, NY", 8175000, 8008000],
+                        ["Los Angeles, CA", 3792000, 3694000],
+                        ["Chicago, IL", 2695000, 2896000],
+                        ["Houston, TX", 2099000, 1953000],
+                        ["Philadelphia, PA", 1526000, 1517000],
+                    ]}
+                    options={{
+                        title: "Population of Largest U.S. Cities",
+                        chartArea: { width: "30%" },
+                        hAxis: {
+                            title: "Total Population",
+                            minValue: 0,
+                        },
+                        vAxis: {
+                            title: "City",
+                        },
+                    }}
+                    legendToggle
+                /> */}
+                <FormDisplay display={display} handleChange={handleChange} style={{maxWidth: 1000, minWidth: 800}}/>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                {display === "Exhibit Information" && <ExhibitInfo />}
+                {display === "Robot Information" && <RobotInfo />}
+                {display === "Daily Information" && <DailyInfo />}
+            </div>
         </div>
     );
-
-}
+};
